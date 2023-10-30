3d23e19677b5c23767231ed489304dc811b9e7da
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-10-30 15:58:26 -0300

    Teste operações

---
+++ primieros-passos/operacoes.php
@@ -0,0 +1,10 @@
+<?php
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisão = 2 / 2;
+$potencia = 2 ** 6;
+$restoDaDivisão = 10 % 3;
+
+echo $soma," ", $subtracao," ", $multiplicacao," ", $divisão," ", $potencia," ", $restoDaDivisão;
