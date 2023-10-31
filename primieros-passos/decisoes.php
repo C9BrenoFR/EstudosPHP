fef26d6bde4b27d0fda59a06230f1810176261ee
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-10-31 13:50:28 -0300

    Testes extras de condições

--- primieros-passos/decisoes.php
+++ primieros-passos/decisoes.php
@@ -2,9 +2,15 @@
 
 $idade = 18;
 
-echo "Você só pode entrar se for maior de 18 anos" . PHP_EOL;
+echo "Você só pode entrar se for maior de 18 anos ou";
+echo "A partir de 16 anos, acompanhado" . PHP_EOL;
 
 if ($idade >= 18) {
     echo "Você tem $idade anos" . PHP_EOL;
     echo "Pode entrar";
+}else {
+    echo "Você só tem $idade. Você não pode entrar";
 }
+
+echo PHP_EOL;
+echo "Adeus!";
